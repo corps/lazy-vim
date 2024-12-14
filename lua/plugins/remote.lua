26fac32b8c7bcd5a24d1089d563d5c89862602cd
Zachary <recursive.cookie.jar@gmail.com>
2024-12-13 16:15:07 -0800

    Adding more plugins

---
+++ lua/plugins/remote.lua
@@ -0,0 +1,21 @@
+return {
+  "amitds1997/remote-nvim.nvim",
+  version = "*", -- Pin to GitHub releases
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- For standard functions
+    "MunifTanjim/nui.nvim", -- To build the plugin UI
+    "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+  },
+  config = {
+    client_callback = function(port, _)
+      local cmd = ("neovide --server localhost:%s"):format(port)
+      vim.fn.jobstart(cmd, {
+        detach = true,
+        on_exit = function(job_id, exit_code, event_type)
+          -- This function will be called when the job exits
+          print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
+        end,
+      })
+    end,
+  },
+}
